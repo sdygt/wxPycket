a8d2d19b2fd9f88a02687889583fb903e93ee2fa
sdygt <root@sdygt.net>
2017-05-08 19:08:17 +0800

    Add GUI

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    pass
+import gettext
+
+import wx
+
+from layout.out.MyFrame import MyFrame
 
 
-if __name__ == '__main__':
-    main()
+class MainFrame(MyFrame):
+    def __init__(self, *args, **kwds):
+        MyFrame.__init__(self, *args, **kwds)
+
+    def OnAbout(self):
+        wx.MessageBox('B14011106', '关于', wx.OK)
+
+
+
+
+if __name__ == "__main__":
+    gettext.install("wxPycket")  # replace with the appropriate catalog name
+
+    wxPycket = wx.App()
+    frame = MainFrame(None, wx.ID_ANY, "")
+    wxPycket.SetTopWindow(frame)
+    frame.Show()
+    wxPycket.MainLoop()
